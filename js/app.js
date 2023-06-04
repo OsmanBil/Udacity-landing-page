fe1d045477b37d313fd9b90d51836e0975098c90
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-06-04 15:59:28 +0200

    feat
    Added Section Active State

--- js/app.js
+++ js/app.js
@@ -68,23 +68,50 @@ function buildNavBar() {
 buildNavBar();
 
 // Scroll to section on link click
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    // Retrieve the navigation menu
     const navBar = document.getElementById('navBar');
-    // Attach a click event listener to the navigation menu
     navBar.addEventListener('click', (event) => {
-        // Check if the clicked element is an anchor tag
         if (event.target.tagName === 'A') {
-            // Prevent the default anchor link behavior
             event.preventDefault();
-            // Retrieve the target section's id from the href attribute
             const targetSectionId = event.target.getAttribute('href');
-            // Retrieve the target section element
             const targetSection = document.querySelector(targetSectionId);
-            // Scroll to the target section using smooth scrolling
             targetSection.scrollIntoView({ behavior: 'smooth' });
         }
     });
-});
-// Set sections as active
 
+    // Run the function on scroll event
+    window.addEventListener('scroll', highlightActiveLink);
+});
+
+// Set sections as active
+function highlightActiveLink() {
+    // Get all section elements
+    const sections = document.querySelectorAll('section');
+    for (let section of sections) {
+        // Get the corresponding navbar link
+        const navLink = document.querySelector(`a[href="#${section.id}"]`);
+
+        if (isInViewport(section)) {
+            // Add 'active' class to the corresponding navbar link
+            navLink.classList.add('active');
+        } else {
+            // Remove 'active' class
+            navLink.classList.remove('active');
+        }
+    }
+
+}
+
+// Function to check if an element is in viewport
+const isInViewport = (element) => {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
